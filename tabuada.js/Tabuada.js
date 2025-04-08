3a1d2c5a083b3688f85095afd557d270c8113d53
Klostermann <166918708+klostermann-ale@users.noreply.github.com>
2025-04-08 11:15:05 -0300

    Salvando o projeto de calculadora de tabuadas

---
+++ tabuada.js/Tabuada.js
@@ -0,0 +1,11 @@
+//Função para exibir a tabuada de um número
+function exibirTabuada(numero) {
+    console.log("Tabuada do " + numero + ":");
+    for ( j= 1; j<= 10; j++) {
+      console.log(numero + "x" + j+ "=" + (numero * j));
+   }
+ }
+ 
+ //Exemplo de uso da função
+ let numeroEscolhido =7;
+ exibirTabuada(numeroEscolhido);
